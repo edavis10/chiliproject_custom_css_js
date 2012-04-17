041c1975b0d3f305c582e47bae482488488e89fb
Eric Davis <edavis@littlestreamsoftware.com>
2012-04-17 10:11:04 -0700

    Create base plugin and settings

---
+++ Rakefile
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+require 'redmine_plugin_support'
+
+Dir[File.expand_path(File.dirname(__FILE__)) + "/lib/tasks/**/*.rake"].sort.each { |ext| load ext }
+
+RedminePluginSupport::Base.setup do |plugin|
+  plugin.project_name = 'chiliproject_custom_css_js'
+  plugin.default_task = [:doc]
+end
---
+++ init.rb
@@ -0,0 +1,19 @@
+require 'redmine'
+
+Redmine::Plugin.register :chiliproject_custom_css_js do
+  name 'Custom CSS and JavaScript'
+  author 'Eric Davis'
+  url 'https://projects.littlestreamsoftware.com'
+  author_url 'http://www.littlestreamsoftware.com'
+  description "Add custom CSS and JavaScript to your ChiliProject"
+  version '1.0.0'
+
+  requires_redmine :version_or_higher => '0.8.0'
+
+  settings({
+             :partial => 'settings/custom_css_js',
+             :default => {
+               'custom_css' => '',
+               'custom_javascript' => ''
+             }})
+end
